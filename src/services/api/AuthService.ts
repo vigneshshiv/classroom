ee8268062b2468952bce9cb738bf6840decdc2bf
vigneshshiv <vichuviky@gmail.com>
2022-10-11 11:05:34 +0530

    API service formatted

--- src/services/api/AuthService.ts
+++ src/services/api/AuthService.ts
@@ -1,11 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 // Application
 import { Data } from '../../shared/base.data';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'admin' })
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'admin' });
 }
