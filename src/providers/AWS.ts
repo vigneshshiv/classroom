f2cd54b5885993415599f98487acff72f65d95e8
vigneshshiv <vichuviky@gmail.com>
2022-10-19 08:38:59 +0530

    AWS SDK dependency added for AWS S3

--- src/providers/AWS.ts
+++ src/providers/AWS.ts
@@ -1,9 +1,13 @@
 /**
  * AWS
  */
+import * as aws from 'aws-sdk';
 
-const AWS = () => {
-  // TODO:
-}
+const AWS = aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  signatureVersion: process.env.AWS_VERSION
+});
 
 export default AWS;
