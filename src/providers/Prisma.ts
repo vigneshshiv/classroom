7a744e4ad500deb0a785a28abf8c7b9c9b942639
vigneshshiv <vichuviky@gmail.com>
2022-10-22 00:08:12 +0530

    chore: Prisma development query log settings changes

--- src/providers/Prisma.ts
+++ src/providers/Prisma.ts
@@ -7,7 +7,13 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = (global.prisma = globalThis.prisma || new PrismaClient());
+const prisma = (global.prisma = globalThis.prisma || 
+  new PrismaClient({
+    log: 
+      Object.is(process.env.NODE_ENV, 'development') 
+        ? ['query', 'error', 'warn'] 
+        : ['error']
+  }));
 if (!Object.is(process.env.NODE_ENV, 'production')) globalThis.prisma = prisma;
 
 export default prisma;
