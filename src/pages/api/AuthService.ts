7c59f33a55f0a984d52ca0be2b613333a09944ac
vigneshshiv <vichuviky@gmail.com>
2022-10-23 09:46:34 +0530

    NextAuth credentials provider implementation - history

--- src/pages/api/AuthService.ts
+++ src/pages/api/AuthService.ts
@@ -1,8 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/**
+ * Next Auth credentails provider Handler
+ */
 import type { NextApiRequest, NextApiResponse } from 'next';
-// Application
-import { Data } from '../../shared/base.data';
+import { getToken } from 'next-auth/jwt';
+import prisma from 'providers/prisma';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'admin' });
+const secret = process.env.JWT_SECRET;
+
+export default async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case 'POST': {
+      const user = await getUser(req, res);
+      // Generate Access Token and Update in Session object
+      console.log(`secret - ${secret}`);
+      const token = await getToken({ req, secret });
+      console.log(`JWT Token - ${JSON.stringify(token)}`);
+      // res.setHeader('Authorization', String(token));
+    }
+  }
+}
+
+const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email } = req.body;
+  console.log(`email - ${email}`);
+  const user = await prisma.user
+    .findUnique({
+      where: {
+        email
+      },
+      select: {
+        id: true,
+        email: true
+      }
+    })
+    .then((data: any) => {
+      console.log(`query response - ${JSON.stringify(data)}`);
+      res.status(200).json(data);
+    });
+    console.log(`Inside User - ${JSON.stringify(user)}`);
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@
  */
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 // Prisma adapter for NextAuth
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from 'providers/prisma';
@@ -12,10 +13,44 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {},
+      async authorize(credentials, req) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const response = await fetch('http://localhost:3000/api/authService', {
+          headers: { 'Content-type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({ email })
+        });
+        return response as any;
+      }
     })
   ],
+
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens
+  secret: process.env.NEXTAUTH_SECRET,
+
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `strategy` should be set to 'jwt' if no database is used.
+    strategy: 'jwt'
+  },
+
+  // JSON Web tokens are only used for sessions if the `strategy: 'jwt'`
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: process.env.JWT_SECRET
+  },
+
   // Authentication adapter
   adapter: PrismaAdapter(prisma),
+
   callbacks: {
     async jwt({ token, user, account, profile, isNewUser }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
@@ -23,9 +58,14 @@ export const authOptions: NextAuthOptions = {
         token.accessToken = account.access_token;
         token.user = user;
       }
+      console.log(`Token - ${JSON.stringify(token)}`);
+      console.log(`User - ${JSON.stringify(user)}`);
+      console.log(`Account - ${JSON.stringify(account)}`);
+      console.log(`Is it called ? - ${token.accessToken}`);
       return token;
     }, 
     async session({ session, user, token}) {
+      console.log(`Does it called on jwt session?`);
       if (session.user) {
         session.user.id = user.id;
         session.user.role = user.role;
@@ -42,7 +82,10 @@ export const authOptions: NextAuthOptions = {
       }
       return baseUrl;
     }
-  }
+  },
+
+  // Enable debug messages in the console if you are having problems
+  debug: false,
 };
 
 export default NextAuth(authOptions);
