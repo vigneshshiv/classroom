0edaba4402a1b2326908a4bd390e635b996c9791
vigneshshiv <vichuviky@gmail.com>
2022-11-10 23:02:02 +0530

    feat: table component added and students adapts to table component

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,45 @@
+/**
+ * Table
+ */
+import React, { ReactNode } from 'react';
+
+const Table = ({ headers, rows }: { headers: string[], rows: ReactNode[][] }): JSX.Element => {
+  return (
+    <table className='table-fixed min-w-full divide-y divide-gray-200 dark:divide-gray-400'>
+      <thead className='bg-gray-100 dark:bg-gray-700'>
+        <tr>
+          {headers.map((header) => (
+            <th 
+              key={header}
+              scope='col'
+              className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'
+            >
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className='divide-y divide-gray-200 dark:divide-gray-500 dark:text-gray-50'>
+        {rows.map((row, rowIndex) => {
+          return (
+            <tr
+              key={rowIndex}
+              className='hover:bg-blue-50 dark:hover:bg-gray-700'
+            >
+              {row.map((column, columnIndex) => (
+                <td
+                  key={columnIndex}
+                  className='py-2.5 px-6 text-sm tracking-wider text-gray-700 dark:text-gray-200'
+                >
+                  {column}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -4,8 +4,27 @@
 
 import { H3 } from 'components/common/Heading';
 import Container from 'components/core/Container';
+import Table from 'components/table/Table';
 
 const Students = (): JSX.Element => {
+
+  const studentsData = [
+    {
+      name: 'Vignesh',
+      email: 'vichuviky@gmail.com',
+      enrolledCourse: 'Front-end development',
+      finishedCourse: 'DevOps and Cloud',
+      stars: '***'
+    },
+    {
+      name: 'Shiv',
+      email: 'shiv@gmail.com',
+      enrolledCourse: 'DevOps Architect',
+      finishedCourse: 'AWS Printical Architect',
+      stars: '****'
+    }
+  ];
+
   return (
     <div className='relative'>
       <Container>
@@ -14,33 +33,22 @@ const Students = (): JSX.Element => {
             <H3 className='mt-8 text-2xl text-gray-800 dark:text-gray-50'>Students Data</H3>
           </div>
           <div className='overflow-x-auto'>
-            <table className='table-fixed min-w-full divide-y divide-gray-200 dark:divide-gray-400'>
-              <thead className='bg-gray-100 dark:bg-gray-700'>
-                <tr>
-                  <th className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'>Student Name</th>
-                  <th className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'>Email</th>
-                  <th className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'>Course Enrolled</th>
-                  <th className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'>Finished Courses</th>
-                  <th className='py-2 px-6 text-left text-sm font-medium tracking-wider text-gray-700 dark:text-gray-200 uppercase'>Stars</th>
-                </tr>
-              </thead>
-              <tbody className='divide-y divide-gray-200 dark:divide-gray-500 dark:text-gray-50'>
-                <tr className='hover:bg-blue-50 dark:hover:bg-gray-700'>
-                  <td className='py-2 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>Vignesh</td>
-                  <td className='py-2 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>vichuviky@gmail.com</td>
-                  <td className='py-2 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>Front-end development</td>
-                  <td className='py-2 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>DevOps and Cloud</td>
-                  <td className='py-2 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>***</td>
-                </tr>
-                <tr className='hover:bg-blue-50 dark:hover:bg-gray-700'>
-                  <td className='py-4 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>Shiv</td>
-                  <td className='py-4 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>shiv@gmail.com</td>
-                  <td className='py-4 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>DevOps Architect</td>
-                  <td className='py-4 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>AWS Printical Architect</td>
-                  <td className='py-4 px-6 text-sm font-medium text-gray-700 dark:text-gray-200'>****</td>
-                </tr>
-              </tbody>
-            </table>
+            <Table 
+              headers={[
+                'Student Name', 
+                'Email', 
+                'Course Enrolled', 
+                'Finished Courses', 
+                'Stars'
+              ]}
+              rows={studentsData.map((student) => [
+                student.name,
+                student.email,
+                student.enrolledCourse,
+                student.finishedCourse,
+                student.stars
+              ])}
+            />
           </div>
         </div>
       </Container>
